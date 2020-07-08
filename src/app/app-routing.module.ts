a06112c63f9807e15833e324c7d19f9577eb45b2
chad svastisalee <swasgrafx@gmail.com>
2020-07-08 14:12:03 -0500

    Generated and routed ArticleCreateComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-// 2. Import the UserComponent
+// 2. Import the Component
 import { UsersComponent }   from './users/users.component';
 import { UserViewComponent }   from './user-view/user-view.component';
 import { UserCreateComponent }   from './user-create/user-create.component';
 import { UserEditComponent }   from './user-edit/user-edit.component';
 import { ArticlesComponent }   from './articles/articles.component';
 import { ArticleViewComponent }   from './article-view/article-view.component';
+import { ArticleCreateComponent }   from './article-create/article-create.component';
 
 // 3. Declare your routes
 const routes: Routes = [
@@ -17,7 +18,8 @@ const routes: Routes = [
   { path: 'users/create', component: UserCreateComponent },
   { path: 'users/edit/:id', component: UserEditComponent },
   { path: 'articles', component: ArticlesComponent },
-  { path: 'articles/view/:id', component: ArticleViewComponent }
+  { path: 'articles/view/:id', component: ArticleViewComponent },
+  { path: 'articles/create', component: ArticleCreateComponent }
 
 ];
 
