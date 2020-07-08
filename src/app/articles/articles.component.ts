565a32978f6a5aa34a98f5522c135556f173b007
chad svastisalee <swasgrafx@gmail.com>
2020-07-08 13:50:05 -0500

    Implemented article service

---
+++ src/app/articles/articles.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+// 1. Import the ArticleService
+import { ArticlesService } from '../articles.service';
+
+// 2. Import the Article Object/Schema
+import { Article } from '../article';
+
+@Component({
+  selector: 'app-articles',
+  templateUrl: './articles.component.html',
+  styleUrls: ['./articles.component.scss']
+})
+export class ArticlesComponent implements OnInit {
+
+  // 3. Create a articles property of type article
+  articles: Article[];
+
+  // 4. Inject the ArticlesService into the constructor
+  constructor(private articlesService: ArticlesService) { }
+
+  // 6. Make a call to the service on initialization
+  ngOnInit() {
+    this.getArticles();
+  }
+
+  // 5. Create a local wrapper for
+  getArticles(): void {
+    this.articlesService.getArticles().subscribe(
+      (response) => {
+        this.articles = response,
+        console.log(this.articles)
+      }
+    );
+  }
+}
