dc00e46dfea0e30897136d6b234d2277482a654b
chad svastisalee <swasgrafx@gmail.com>
2020-07-13 08:49:13 -0500

    Generate User Service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../services/user.service';
+
+// 1. Import the User object (model)
+import { User } from '../models/user';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  // 2. Create a new instance of the User object
+  user:User = new User();
+  // 3. Instantiate an errors array
+  error: any;
 
-  constructor() { }
+  // 4. Clear out the contructor login
+  constructor(private userService: UserService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {}
+
+  // 5. Add a login method
+  login(): void{
+
+    this.userService.login(this.user).subscribe(
+      (response:any)=>{
+        console.log(response);
+
+        if(response.success == false){
+          this.error=true;
+        }
+
+      }
+    );
   }
 
 }
